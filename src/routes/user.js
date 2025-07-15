49f7f37d0437e8ebfbf4e5fc26feb77556571f52
rajesh.shah <rajesh.shah@lifeline.com>
2025-07-15 14:37:27 +0530

    Feed api aded with pagination

--- src/routes/user.js
+++ src/routes/user.js
@@ -3,6 +3,7 @@ const userRouter = express.Router();
 
 const { userAuth } = require("../middleware/auth"); // Import user authentication middleware
 const ConnectionRequestModel = require("../models/connectionRequest");
+const User = require("../models/user");
 
 const userAllowedField = "firstName lastName skills about photoUrl";
 // get all pending conenction requests
@@ -52,6 +53,41 @@ userRouter.get("/connections", userAuth, async (req, res) => {
     return;
   } catch (error) {
     res.send("Error occured:" + error.message);
+  }
+});
+
+userRouter.get("/feed", userAuth, async (req, res) => {
+  try {
+    const { user: loggedInUser } = req;
+    const page = req.query.page || 1;
+    let limit = req.query.limit || 10;
+
+    limit = limit > 50 ? 50 : limit;
+
+    const allRequest = await ConnectionRequestModel.find({
+      $or: [{ fromUserId: loggedInUser._id }, { toUserId: loggedInUser._id }],
+    }).select("fromUserId toUserId");
+
+    const skip = (page - 1) * limit;
+    const hideUsers = new Set();
+
+    allRequest.forEach((request) => {
+      hideUsers.add(request.fromUserId.toString());
+      hideUsers.add(request.toUserId.toString());
+    });
+
+    const feedItems = await User.find({
+      $and: [
+        { _id: { $nin: Array.from(hideUsers) } },
+        { _id: { $ne: loggedInUser._id } },
+      ],
+    })
+      .select(userAllowedField)
+      .skip(skip)
+      .limit(limit);
+    res.status(200).send(feedItems);
+  } catch (error) {
+    res.status(500).json({ message: `Error: ${error.message}` });
   }
 });
 
